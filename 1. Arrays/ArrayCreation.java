434290ef88691534c2c2d8034ece17624c24bb54
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2025-03-02 09:57:46 +0000

    1.1.

---
+++ 1. Arrays/ArrayCreation.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class ArrayCreation {
+    public static void main(String args[]) {
+        int marks[] = new int[50];
+
+        int num[] = {1,2,3};
+
+        int moreNum[] = {4,5,6};
+
+        String fruits[] = {"apple", "mango"};
+
+        System.out.println(Arrays.toString(marks));
+        System.out.println(Arrays.toString(num));
+        System.out.println(Arrays.toString(moreNum));
+        System.out.println(Arrays.toString(fruits));
+    }
+}
