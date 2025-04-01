bf2d670bcfc627ed4530dac4bb6aca8dbf9d08bc
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2025-04-01 19:15:34 +0000

    0.2.6.

---
+++ 0.2. Operators/Ternary.java
@@ -0,0 +1,8 @@
+public class Ternary {
+    public static void main(String[] args) {
+        int n = 7;
+
+        String type = ((n%2) == 0) ? "even" : "odd";
+        System.out.println(type);
+    }
+}
