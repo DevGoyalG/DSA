cbaf50ca321dffd9950e9542174c44ea4b601f9f
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2025-04-01 18:26:54 +0000

    0.1.3.

---
+++ 0.1. Variable & Data Structure/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 18;
+        System.out.println(a);
+        System.out.println(b);
+        String x = "Rohit Sharma";
+        String y = "Virat Kohli";
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
