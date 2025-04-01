40d8c24c5a7504037a89754506a90d43180815d0
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2025-04-01 19:16:31 +0000

    0.3.7.

---
+++ 0.3. Conditional Statement/PassOrFail.java
@@ -0,0 +1,8 @@
+public class PassOrFail {
+    public static void main(String[] args) {
+        int marks = 31;
+
+        String report = marks >= 33 ? "pass" : "fail";
+        System.out.println(report);
+    }
+}
