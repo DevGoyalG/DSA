1598b3de7b2c6835de6c27c9388b13bdc3168015
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2025-04-01 18:56:01 +0000

    0.3.3.

---
+++ 0.3. Conditional Statement/OddEven.java
@@ -0,0 +1,19 @@
+// package Conditional Statement;
+
+public class OddEven {
+    public static void main(String[] args) {
+        int a = 45;
+
+        if(a%2==0){
+            System.out.println("Even");
+        }
+
+        else{
+            System.out.println("Odd");
+        }
+
+        // else{
+        //     System.out.println("None");
+        // }
+    }
+}
