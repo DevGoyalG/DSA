d15cbf2f97db025c4847ce8faa83051d176eae60
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2025-04-01 18:27:46 +0000

    0.1.4.

---
+++ 0.1. Variable & Data Structure/DataTypes.java
@@ -0,0 +1,29 @@
+public class DataTypes {
+    public static void main(String [] args){
+
+        //Primitive DT
+
+        byte b = 8;
+        System.out.println(b);
+
+        char ch = 'a';
+        System.out.println(ch);
+
+        boolean var = false;
+        System.out.println(var);
+
+        // float price = 0.5;
+        // System.out.println(float);
+
+        int n = 10;
+        System.out.println(n);
+
+        //long
+        //double
+        //short
+
+        //Non Primitive DT
+
+        
+    }
+}
