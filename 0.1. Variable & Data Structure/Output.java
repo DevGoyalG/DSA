bce620f35a5c77d873bdc0a8920b072911ca63df
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2025-04-01 18:24:49 +0000

    0.1.1.

---
+++ 0.1. Variable & Data Structure/Output.java
@@ -0,0 +1,7 @@
+public class Output{
+    public static void main(String[] args) {
+        System.out.println("Rohit Sharma");
+        System.out.println("Virat Kohli");
+        System.out.println("MS Dhoni");
+    }
+}
