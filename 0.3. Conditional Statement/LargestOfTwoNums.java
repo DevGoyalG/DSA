39080d861ea271f1fd8094074d13adcb48492e37
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2025-04-01 18:55:02 +0000

    0.3.2.

---
+++ 0.3. Conditional Statement/LargestOfTwoNums.java
@@ -0,0 +1,19 @@
+// package Conditional Statement;
+
+public class LargestOfTwoNums {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 1;
+        if(a>b){
+            System.out.println("a is big");
+        }
+
+        if (b>a){
+            System.out.println("b is big");
+        }
+
+        else{
+            System.out.println("A is equal to b");
+        }
+    }
+}
