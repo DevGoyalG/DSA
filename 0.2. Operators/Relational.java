b1254258d484ef898c3a1107dbd89d6a45911d5d
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2025-04-01 18:46:12 +0000

    0.2.3.

---
+++ 0.2. Operators/Relational.java
@@ -0,0 +1,13 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+
+public class Relational {
+    public Relational() {
+    }
+ 
+    public static void main(String[] var0) {
+       byte var1 = 10;
+       byte var2 = 5;
+       System.out.println(var1 == var2);
+    }
+ }
+ 
