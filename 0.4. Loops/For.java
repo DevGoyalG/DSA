288946364dc9b5755b49d44c0742c4514a0f55c6
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2025-04-02 04:27:07 +0000

    0.4.5.

---
+++ 0.4. Loops/For.java
@@ -0,0 +1,12 @@
+// import java.util.Scanner;
+
+public class For {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // int i = sc.nextInt();
+        for(int i=1; i<=10; i++){
+            System.out.println("RO");
+        }
+        // sc.close();
+    }
+}
