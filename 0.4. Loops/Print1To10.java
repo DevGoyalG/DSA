8fe715732b22a5d0d33b3cc35a6e83f9c5d8cb3f
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2025-04-02 04:16:48 +0000

    0.4.2.

---
+++ 0.4. Loops/Print1To10.java
@@ -0,0 +1,18 @@
+// public class Print1to10 {
+//     public static void main(String[] args) {
+//         int counter = 1;
+//         while(counter <= 10){
+//             System.out.println(counter);
+//             counter++;
+//         }
+//     }
+// }
+
+
+public class Print1To10 {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+}
