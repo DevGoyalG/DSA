c5b62b9ccee6fcd2839762fa64c4707ccef9aceb
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2025-04-02 04:59:09 +0000

    0.4.9.

---
+++ 0.4. Loops/DoWhile.java
@@ -0,0 +1,9 @@
+public class DoWhile{
+    public static void main(String[] args) {
+        int counter = 1;
+        do{
+            System.out.println("RO");
+            counter++;
+        } while(counter<=10);
+    }
+}
