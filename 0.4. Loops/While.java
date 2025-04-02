b1bdfc31cdbe52047b69db0ee5ede614d40cb98b
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2025-04-02 04:14:29 +0000

    0.4.1.

---
+++ 0.4. Loops/While.java
@@ -0,0 +1,13 @@
+// package Loops;
+
+// print Rohit Sharma 100 times
+
+public class While {
+    public static void main(String[] args) {
+        int counter = 0;
+        while(counter < 100){
+            System.out.println("Rohit Sharma");
+            counter++;
+        }
+    }
+}
