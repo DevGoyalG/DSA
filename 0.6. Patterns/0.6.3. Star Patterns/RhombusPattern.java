877f58d6caaac3c53bcd6f8863da2f1bb4b9dfb3
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2025-04-02 16:52:58 +0000

    0.6.3.12.

---
+++ 0.6. Patterns/0.6.3. Star Patterns/RhombusPattern.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class RhombusPattern {
+    public static void solid_rhombus(int n){
+        for(int i=1; i<=n; i++){
+            //space
+            for(int j=1; j<=(n-i); j++){
+                System.out.print(" ");
+            }
+
+            //star
+            for(int j=1; j<=n; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt(); 
+
+        solid_rhombus(n);
+
+        sc.close();
+    }
+}
