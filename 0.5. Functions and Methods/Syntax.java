72d82fd22c6da8284f2a9aa58e246ce839b682c8
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2025-04-02 05:21:04 +0000

    0.5.1.

---
+++ 0.5. Functions and Methods/Syntax.java
@@ -0,0 +1,10 @@
+public class Syntax {
+    public static void printRO(){
+     System.out.println("RO45");
+     return;
+    }
+
+    public static void main(String[] args) {
+        printRO();
+    }
+}
