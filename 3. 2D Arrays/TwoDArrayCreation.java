d5367b260c0262cda56642e20ba62374cb1209e4
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2025-04-20 07:33:37 +0000

    3.1.

--- 3. 2D Arrays/TwoDArrayCreation.java
+++ 3. 2D Arrays/TwoDArrayCreation.java
@@ -3,7 +3,8 @@ import java.util.*;
 public class TwoDArrayCreation {
     public static void main(String[] args){
         int matrix[][] = new int[3][3];
-        int n = matrix.length, m = matrix[0].length;
+        int n = matrix.length;                 // no of rows
+        int m = matrix[0].length;              // no of cols
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter numbers in array: ");
@@ -17,7 +18,7 @@ public class TwoDArrayCreation {
 
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                System.out.print(matrix[i][j]);
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
